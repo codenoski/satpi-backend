939b3382fef8689c5f6281cb5324cfee4b8b0fa7
codenoski <153275941+codenoski@users.noreply.github.com>
2026-04-21 22:20:05 +0200

    Add files via upload

---
+++ backend_localGV.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+latest_data = None
+history = []
+
+
+class Telemetry(BaseModel):
+    temps: float
+    alt: float
+    temp: float
+    press: float
+    lat: float
+    lon: float
+    rssi: float
+    servoX: int
+    servoY: int
+
+
+@app.get("/")
+def root():
+    return {"ok": True, "message": "Backend actiu"}
+
+
+@app.post("/telemetry")
+def receive_telemetry(data: Telemetry):
+    global latest_data, history
+
+    latest_data = data.dict()
+    history.append(latest_data)
+
+    if len(history) > 500:
+        history = history[-500:]
+
+    return {"ok": True}
+
+
+@app.get("/telemetry/latest")
+def get_latest():
+    return latest_data if latest_data else {}
+
+
+@app.get("/telemetry/history")
+def get_history():
+    return history
